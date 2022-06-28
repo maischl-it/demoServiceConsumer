f40cffc7e11f7fa7bd595476bba503eaeb0e3cf1
Markus Jung <markus.jung@ivfp.de>
2022-06-28 13:21:50 +0200

    changes

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 import requests
 import os
+import threading
+import time
 
 app = Flask(__name__)
 
+metricsValue = 0
 
 @app.route("/", methods=['POST', 'GET'])
 def home():
+    global metricsValue
+    metricsValue += 1
 
     url = os.environ.get("url", "http://provider.default.svc.cluster.local")
     # response = requests.get("http://127.0.0.1:3000")
@@ -18,8 +23,19 @@ def home():
 
 @app.route("/metrics", methods=['POST', 'GET'])
 def metrics():
-    return '{"value":8}'
+    return '{"value":' + str(metricsValue) + '}'
 
+
+def decrement_metrics():
+    global metricsValue
+
+    while True:
+        if (metricsValue > 0):
+            metricsValue -= 1
+
+        time.sleep(10)
 
 if __name__ == '__main__':
+    threading.Thread(target=decrement_metrics).start()
+
     app.run(host='0.0.0.0', port=3001)
